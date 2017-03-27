23291be1eda1c0f69c1f2daf24dbb63efd6925dd
KenCarroll <ken.carroll@bumpconductor.com>
2017-03-27 11:49:42 +0200

    intermim code for solvers domain firebase integration

--- app/src/service.js
+++ app/src/service.js
@@ -7,6 +7,7 @@ class Service {
     firebase.auth().onAuthStateChanged(
       (user) => store.dispatch(app.auth.setUser(user)),
     );
+    //Service.solversRef = new firebase.database().ref("solvers/");
   }
 
   static signInWithGoogle() {
@@ -21,6 +22,37 @@ class Service {
   static signOut() {
     return firebase.auth().signOut();
   }
-}
 
+//adding SOLVERS (problem solving orgs)
+
+  static monitorSolverUpdates() {
+    const solversRef = new firebase.database().ref('solvers/');
+    solversRef.on('value', (snapshot) => 
+      store.dispatch(app.solvers.solversUpdate(snapshot.val())));
+  }
+
+  static addSolver(solver) {
+    console.log(`service: addSolver : ${solver}`);
+    var creatorUID = firebase.auth().currentUser.uid;
+    var creatorName = firebase.auth().currentUser.displayName;
+    var solverData = {
+      orgName : solver.orgName,
+      orgURL : solver.orgURL,
+      contactName : solver.contactName,
+      contactEmail : event.detail.contactEmail,
+      tags : solver.tags,
+      creatorUID: creatorUID,
+      creatorName: creatorName,
+      created: firebase.database.ServerValue.TIMESTAMP,
+      updated: firebase.database.ServerValue.TIMESTAMP
+    };
+
+    var newSolverKey = firebase.database().ref().child('solvers').push().key;
+    
+    var updates = {};
+    updates['/solvers/' + newSolverKey] = solverData;
+    firebase.database().ref().update(updates);
+  }
+
+}
 export default Service;
